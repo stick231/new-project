48e6545f3f24f6051d18d4a29f97718bb50391d3
stick231 <veniamin7766z@gmail.com>
2024-09-22 21:27:45 +0300

    added composer, routing, autoloader, auth and register

--- www/index.php
+++ www/index.php
@@ -1,3 +1,61 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+session_start();
 
-phpinfo();
+use Controllers\AuthController;
+use Controllers\AuthMiddleWare;
+use Phroute\Phroute\Dispatcher;
+use Phroute\Phroute\RouteCollector;
+
+$router = new RouteCollector();
+
+$authAction = new AuthController();
+
+$authMiddleWare = new AuthMiddleWare();
+
+$router->get('/', function() use ($authMiddleWare){
+    $authMiddleWare->handle($_REQUEST, function(){
+
+    });
+});
+
+$router->any('/register', function() use ($authAction){
+    $authAction->redirectToRegisterPage();
+});
+
+$router->any('/auth', function() use ($authAction){
+    $authAction->redirectToAuthPage();
+});
+
+$dispatcher = new Dispatcher($router->getData());
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = strtok($_SERVER['REQUEST_URI'], '?');
+
+try {
+    echo $dispatcher->dispatch($httpMethod, $uri);
+} catch (Phroute\Phroute\Exception\HttpMethodNotAllowedException $e) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed");
+    echo 'Метод не разрешен: ' . $e->getMessage();
+    exit;
+} catch (Phroute\Phroute\Exception\HttpRouteNotFoundException $e) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    echo 'Маршрут не найден: ' . $e->getMessage();
+    exit;
+} catch (Exception $e) {
+    echo 'Произошла ошибка: ' . $e->getMessage();
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ www/src/Entities/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Entities;
+
+class User{
+    private $username;
+    private $password;
+
+    public function getUsername() 
+    {
+        return $this->username;
+    }
+
+    public function getPassword() 
+    {
+        return $this->password;
+    }
+
+    public function setUsername($username)
+    {
+        $new = clone $this;
+        $new->username = $username;
+        return $new;
+    }
+
+    public function setPassword($password)
+    {
+        $new = clone $this;
+        $new->password = $password;
+        return $new;
+    }
+}
